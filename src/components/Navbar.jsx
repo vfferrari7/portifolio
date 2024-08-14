cb82d936e6f5678523f20d29d3271440d20ca6db
vfferrari7 <vzlkff@outlook.com>
2024-08-14 10:04:47 -0300

    feat: added some style

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import logo from "../assets/logo.png"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub} from "react-icons/fa";
+
+const Navbar = () => {
+  return <nav className="mb-20 flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-16" src={logo} alt="logo"/>
+    </div>
+    <div className="mb-8 mr-4 flex items-center justify-content gap-4 text-2xl">
+        <FaLinkedin/>
+        <FaGithub/>
+    </div>
+  </nav>;
+};
+export default Navbar;

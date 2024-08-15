f5c8f7454413e2fe1568c112cc87e4cad0755c89
vfferrari7 <vzlkff@outlook.com>
2024-08-15 14:54:47 -0300

    style: finished the animations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,14 @@
-import { CONTACT } from "../constants"
+import { CONTACT } from "../constants";
+import {motion} from "framer-motion";
 
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-      <h2 className="text-center text-4xl">Contact</h2>
+      <motion.h2 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-100}} transition={{duration:0.5}} className="text-center text-4xl">Contact</motion.h2>
       <div className="text-center tracking-tighter">
-        <p className="my-4 ">{CONTACT.address}</p>
-        <p className="my-4">{CONTACT.phoneNo}</p>
-        <button onClick={() =>  navigator.clipboard.writeText('vzlkff@gmail.com')} className="my-4 border-b" href=""><p>{CONTACT.email}</p></button>
+        <motion.p whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className="my-4 ">{CONTACT.address}</motion.p>
+        <motion.p whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className="my-4">{CONTACT.phoneNo}</motion.p>
+        <motion.button whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} onClick={() =>  navigator.clipboard.writeText('vzlkff@gmail.com')} className="my-4 border-b" href=""><p>{CONTACT.email}</p></motion.button>
       </div>
     </div>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,17 @@
 import { PROJECTS } from "../constants";
+import {motion} from "framer-motion"
 
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl"></h2>
+      <motion.h2 whileInView={{opacity:1, y:0}} initial={{opacity:0, y:-100}} transition={{duration:0.5}} className="my-20 text-center text-4xl">Projects</motion.h2>
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div>
+            <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}}>
               <img src={project.image} width={500} height={500} className="mb-6 rounded" alt={project.title} />
-            </div>
-            <div className="mx-8 mt-8 text-center text-2xl w-full max-w-xl">
+            </motion.div>
+            <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:1}} className="mx-8 mt-8 text-center text-2xl w-full max-w-xl">
               <h4 className="mb-2 font-semibold">{project.title}</h4>
               <p className="mb-4 text-neutral-400">{project.description}</p>
               {project.technologies.map((tech, index)=>(
@@ -18,7 +19,7 @@ const Projects = () => {
                   {tech}
                 </span>
               ))}
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>

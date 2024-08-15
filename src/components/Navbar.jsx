21b6eb19bc2a028fb86e22a310e57ea4325e9ab7
vfferrari7 <vzlkff@outlook.com>
2024-08-15 15:03:57 -0300

    style:changed the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import logo from "../assets/logo.png"
+
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub} from "react-icons/fa";
 
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-16" src={logo} alt="logo"/>
+        <h2 className="mx-2 w-3/4">VF</h2>
     </div>
     <div className="mb-8 mr-4 flex items-center justify-content gap-4 text-2xl">
         <a className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-blue-400 duration-300" target="blank" href="https://www.linkedin.com/in/viniciusferucciferrari/"><FaLinkedin/></a>

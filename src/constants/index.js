7d9c5257a9cb4e14a07f632445a3cbcc28ae6d05
vfferrari7 <vzlkff@outlook.com>
2024-08-15 10:55:39 -0300

    feat: added the projects description

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,71 +1,41 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from "../assets/projects/serenatto.jpg";
+import project2 from "../assets/projects/fav_games.png";
+import project3 from "../assets/projects/xmen-project.png";
+import project4 from "../assets/projects/sinopse-filme.png";
 
 export const HERO_CONTENT = `I am a passionate front-end developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React and BootStrap, as well as back-end technologies like Node.js. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile front-end developer with a passion for creating efficient and user-friendly web applications. With 1 year of professional experience, I have worked with a variety of technologies, including React, Node.js, TailwindCSS and Bootstrap. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Coffee shop Web-site",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A fully functional website with features like product listing, offered services and forms to contact the owners.",
+    technologies: ["HTML", "CSS", "Bootstrap", "Js"],
   },
   {
-    title: "Task Management App",
+    title: "Game list Website",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "A webpage listing your favorite games, where each game cover is displayed as an image. When users click on a game cover, it will take them to another website with more information about that specific game. This allows users to explore details, reviews, and content related to each game.",
+    technologies: ["HTML", "CSS"],
   },
   {
-    title: "Portfolio Website",
+    title: "Superheroes Characters Website",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "This website is a comprehensive hub for superhero fans, featuring a list of iconic heroes. The design is colorful and dynamic, reflecting the vibrant world of superheroes.",
+    technologies: ["HTML", "CSS", "Js"],
   },
   {
-    title: "Blogging Platform",
+    title: "Movie synopsis Website",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "This website is dedicated to a single film, offering a focused and immersive experience for users interested in that specific movie. The design is sleek and cinematic, with a full-page background featuring a still image from the film.",
+    technologies: ["HTML", "CSS", "Js"],
   },
 ];
 

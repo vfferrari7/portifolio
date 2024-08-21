3098325d61d59c4fc72683ee39e692b288d63394
vfferrari7 <vzlkff@outlook.com>
2024-08-21 10:06:40 -0300

    changed the email feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub} from "react-icons/fa";
+import { MdGTranslate } from "react-icons/md";
+
 
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
@@ -8,6 +10,7 @@ const Navbar = () => {
         <h2 className="mx-2 w-3/4">VF</h2>
     </div>
     <div className="mb-8 mr-4 flex items-center justify-content gap-4 text-2xl">
+        <a className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-blue-400 duration-300 cursor:pointer"><MdGTranslate/></a>
         <a className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-blue-400 duration-300" target="blank" href="https://www.linkedin.com/in/viniciusferucciferrari/"><FaLinkedin/></a>
         <a className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" target="blank" href="https://github.com/vfferrari7"><FaGithub/></a>
     </div>
